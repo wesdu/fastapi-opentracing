fbe92facdb8da84f1f077fa144776dd86aa3b199
duwei <wei.du@tenclass.com>
2020-09-29 01:28:43 +0800

    feat: version 0.2

--- fastapi_opentracing/__init__.py
+++ fastapi_opentracing/__init__.py
@@ -7,7 +7,7 @@ from opentracing.propagation import Format
 
 
 tracer = Tracer(
-    one_span_per_rpc=True,
+    one_span_per_rpc=False,
     service_name='wk_api_gateway',
     reporter=NullReporter(),
     sampler=ConstSampler(decision=True),
