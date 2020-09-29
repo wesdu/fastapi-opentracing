4993630150c86cd166720dd2eb5da0e16ecd1553
duwei <wei.du@tenclass.com>
2020-09-29 17:32:07 +0800

    fix: readme

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def read_requirements(filename):
 
 setuptools.setup(
     name="fastapi_opentracing",
-    version="0.3",
+    version="0.3.1",
     author="Du Wei",
     author_email="pandorid@gmail.com",
     description="fastapi opentracing middleware works with istio",
