2654136e96bd6f8e6b1f94bcb21c384fbdf6ae08
yangpeng <peng.yang@lizhiweike.com>
2021-05-13 21:53:03 +0800

    update default extra header

--- fastapi_opentracing/middleware.py
+++ fastapi_opentracing/middleware.py
@@ -13,69 +13,52 @@ DispatchFunction = typing.Callable[
 
 
 class OpenTracingMiddleware(BaseHTTPMiddleware):
-    _extra_headers = {}
+    _extra_headers = [
+        # All applications should propagate x-request-id. This header is
+        # included in access log statements and is used for consistent trace
+        # sampling and log sampling decisions in Istio.
+        'x-request-id',
 
-    def __init__(self, app: ASGIApp, dispatch: DispatchFunction = None, config: bool = True) -> None:
-        self.app = app
-        self.dispatch_func = self.dispatch if dispatch is None else dispatch
-        if config:
-            self.set_extra_headers()
+        # Lightstep tracing header. Propagate this if you use lightstep tracing
+        # in Istio (see
+        # https://istio.io/latest/docs/tasks/observability/distributed-tracing/lightstep/)
+        # Note: this should probably be changed to use B3 or W3C TRACE_CONTEXT.
+        # Lightstep recommends using B3 or TRACE_CONTEXT and most application
+        # libraries from lightstep do not support x-ot-span-context.
+        'x-ot-span-context',
 
-    def set_extra_headers(self):
-        incoming_headers = [
-            # All applications should propagate x-request-id. This header is
-            # included in access log statements and is used for consistent trace
-            # sampling and log sampling decisions in Istio.
-            'x-request-id',
+        # Datadog tracing header. Propagate these headers if you use Datadog
+        # tracing.
+        'x-datadog-trace-id',
+        'x-datadog-parent-id',
+        'x-datadog-sampling-priority',
 
-            # Lightstep tracing header. Propagate this if you use lightstep tracing
-            # in Istio (see
-            # https://istio.io/latest/docs/tasks/observability/distributed-tracing/lightstep/)
-            # Note: this should probably be changed to use B3 or W3C TRACE_CONTEXT.
-            # Lightstep recommends using B3 or TRACE_CONTEXT and most application
-            # libraries from lightstep do not support x-ot-span-context.
-            'x-ot-span-context',
+        # W3C Trace Context. Compatible with OpenCensusAgent and Stackdriver Istio
+        # configurations.
+        'traceparent',
+        'tracestate',
 
-            # Datadog tracing header. Propagate these headers if you use Datadog
-            # tracing.
-            'x-datadog-trace-id',
-            'x-datadog-parent-id',
-            'x-datadog-sampling-priority',
+        # Cloud trace context. Compatible with OpenCensusAgent and Stackdriver Istio
+        # configurations.
+        'x-cloud-trace-context',
 
-            # W3C Trace Context. Compatible with OpenCensusAgent and Stackdriver Istio
-            # configurations.
-            'traceparent',
-            'tracestate',
+        # Grpc binary trace context. Compatible with OpenCensusAgent nad
+        # Stackdriver Istio configurations.
+        'grpc-trace-bin',
 
-            # Cloud trace context. Compatible with OpenCensusAgent and Stackdriver Istio
-            # configurations.
-            'x-cloud-trace-context',
+        # b3 trace headers. Compatible with Zipkin, OpenCensusAgent, and
+        # Stackdriver Istio configurations. Commented out since they are
+        # propagated by the OpenTracing tracer above.
+        # 'x-b3-traceid',
+        # 'x-b3-spanid',
+        # 'x-b3-parentspanid',
+        # 'x-b3-sampled',
+        # 'x-b3-flags',
 
-            # Grpc binary trace context. Compatible with OpenCensusAgent nad
-            # Stackdriver Istio configurations.
-            'grpc-trace-bin',
-
-            # b3 trace headers. Compatible with Zipkin, OpenCensusAgent, and
-            # Stackdriver Istio configurations. Commented out since they are
-            # propagated by the OpenTracing tracer above.
-            # 'x-b3-traceid',
-            # 'x-b3-spanid',
-            # 'x-b3-parentspanid',
-            # 'x-b3-sampled',
-            # 'x-b3-flags',
-
-            # Application-specific headers to forward.
-            'user-agent',
-        ]
-        # For Zipkin, always propagate b3 headers.
-        # For Lightstep, always propagate the x-ot-span-context header.
-        # For Datadog, propagate the corresponding datadog headers.
-        # For OpenCensusAgent and Stackdriver configurations, you can choose any
-        # set of compatible headers to propagate within your application. For
-        # example, you can propagate b3 headers or W3C trace context headers with
-        # the same result. This can also allow you to translate between context
-        # propagation mechanisms between different applications.
-        self._extra_headers = incoming_headers
+        # Application-specific headers to forward.
+        'user-agent',
+        'x-weike-node',
+    ]
 
     async def dispatch(self, request, call_next):
         try:
