82644c5cc13fbea923c67c7a3632f4ca3ae9e64f
mizhexiaoxiao <54677442+mizhexiaoxiao@users.noreply.github.com>
2021-12-30 17:36:10 +0800

    response header add traceid and sampled (#22)

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read_requirements(filename):
 
 setuptools.setup(
     name="fastapi_opentracing",
-    version="0.6.9",
+    version="0.7.0",
     author="Du Wei",
     author_email="pandorid@gmail.com",
     description="fastapi opentracing middleware works with istio",
